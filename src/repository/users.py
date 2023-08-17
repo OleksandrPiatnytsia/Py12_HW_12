eb19307387a7281a5b651e4279c0cf44a93a9e81
Oleksandr <115171014+OleksandrPiatnytsia@users.noreply.github.com>
2023-08-17 15:17:08 +0300

    user processing db added

---
+++ src/repository/users.py
@@ -0,0 +1,39 @@
+import logging
+
+from libgravatar import Gravatar
+from sqlalchemy import select
+from sqlalchemy.orm import Session
+
+from src.database.db import get_db
+
+from src.database.models import User
+from src.schemas import UserSchema
+
+from fastapi import Depends
+
+
+def get_user_by_email(email: str, session: Session = Depends(get_db)) -> User:
+    sq = select(User).filter_by(email=email)
+    result = session.execute(sq)
+    user = result.scalar_one_or_none()
+    logging.info(user)
+    return user
+
+
+def create_user(body: UserSchema, session: Session = Depends(get_db)) -> User:
+    avatar = None
+    try:
+        g = Gravatar(body.email)
+        avatar = g.get_image()
+    except Exception as e:
+        logging.error(e)
+    new_user = User(**body.model_dump(), avatar=avatar)  # User(username=username, email=email, password=password)
+    session.add(new_user)
+    session.commit()
+    session.refresh(new_user)
+    return new_user
+
+
+def update_token(user: User, token: str | None, session: Session = Depends(get_db)) -> None:
+    user.refresh_token = token
+    session.commit()
--- src/schemas.py
+++ src/schemas.py
@@ -14,3 +14,26 @@ class ContactSchema(BaseModel):
 
 class ContactSchemaResponse(ContactSchema):
     id: str = Field('1', min_length=1, max_length=36)
+
+
+class UserSchema(BaseModel):
+    username: str = Field(min_length=5, max_length=16)
+    email: EmailStr
+    password: str = Field(min_length=6, max_length=16)
+
+
+class UserResponseSchema(BaseModel):
+    id: int
+    username: str
+    email: str
+    avatar: str
+
+    class Config:
+        from_attributes = True
+
+
+class TokenModel(BaseModel):
+    access_token: str
+    refresh_token: str
+    token_type: str = "bearer"
+
